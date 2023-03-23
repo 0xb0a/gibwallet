eb983053f5071b047ca146f56116b66c26cdef9a
qudo <59782726+qudo-code@users.noreply.github.com>
2023-03-22 22:07:57 -0700

    feat: first

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import base58 from "bs58";
+import chalk from "chalk";
+import { TipLink } from "@tiplink/api";
+(async () => {
+    const link = await TipLink.create();
+    console.log(`\nPrivate Key (Unit8Array)\n${chalk.bold.red(JSON.stringify(Object.values(link.keypair.secretKey)))}\n`);
+    console.log(`\nPrivate Key (Base58)\n${chalk.bold.red(base58.encode(Object.values(link.keypair.secretKey)))}\n`);
+    console.log(`\nPublic Key\n${chalk.bold.green(link.keypair.publicKey.toBase58())}\n`);
+    console.log(`\n🌐 Tiplink URL\n${chalk.bold.blue(link.url)}\n`);
+})();
